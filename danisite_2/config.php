4947a93286260ec9b4ee1c9e201c98ba0be8a5a0
DNere <daniel.nere780@gmail.com>
2024-12-07 18:46:02 -0300

    Update config.php

--- danisite_2/config.php
+++ danisite_2/config.php
@@ -4,12 +4,9 @@ $username = "root";
 $password = "0000"; 
 $dbname = "plumasecia";
 
-// Criando a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificando a conexão
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error); // Se a conexão falhar, exibe erro e termina o script
+    die("Connection failed: " . $conn->connect_error);
 }
-// A conexão foi bem-sucedida, mas não imprima diretamente nada se não for necessário
 ?>
