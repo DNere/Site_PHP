6baed6cfded1ab634c713e3c4374c6bbc72a29f1
DNere <daniel.nere780@gmail.com>
2024-12-07 22:28:58 -0300

    plumasecia.sql

--- plumasecia.sql
+++ plumasecia.sql
@@ -1,5 +1,5 @@
-create database plumasecia;
-use plumasecia;
+CREATE database plumasecia;
+USE plumasecia;
 
 CREATE TABLE formulario (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,4 +8,23 @@ CREATE TABLE formulario (
     senha VARCHAR(100)
 );
 
-select * from formulario;
+SELECT * FROM formulario;
+
+INSERT INTO formulario VALUES 
+('001','daniel.nere780@gmail.com','Daniel Nere Rodrigues','senhasecreta'),
+('002','eduarda.rodrigues@gmail.com','Eduarda Nere Rodrigues','2004'),
+('003','a.ghiraldelli@aluno.ifsp.edu.br','Ana Julia Gomes Ghiraldelli','09shd32A638');
+
+
+
+SELECT formulario.nome AS 'Usuários', formulario.email AS 'Emails'
+	FROM formulario
+		ORDER BY formulario.nome;
+
+SELECT formulario.nome AS 'Usuários chamados Daniel'
+	FROM formulario
+		WHERE nome LIKE '%Daniel%';
+        
+SELECT formulario.email AS 'Emails'
+	FROM formulario
+		WHERE email LIKE '%gmail.com%';
