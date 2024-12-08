3fdb990fc02952c699cbeecc1600fc493e51ac78
DNere <daniel.nere780@gmail.com>
2024-12-07 22:35:35 -0300

    javascript.js

--- danisite_2/js/javascript.js
+++ danisite_2/js/javascript.js
@@ -1,34 +1,9 @@
-function criar(){
-
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-    var email = document.getElementById('email').value;
-
-    if(login == "daniel" && senha == "senhasupersecreta" && email == "dani@gmail.com"){
-        alert('Sucesso');
-        location.href = "index.html";
-    }else{
-        alert('Usuario, email ou senha incorretos');
+// Mostra o botão ao descer a tela
+window.onscroll = function() {
+    const button = document.querySelector('.botao-topo');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.classList.add('show');
+    } else {
+        button.classList.remove('show');
     }
-}
-
-// a function criar é responsável por cadastrar um novo usuário, utilizando o email, nome de usuário e a sua senha.
-// esta simples por que eu não tive tempo :(
-
-function logar(){
-
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-
-    if(login == "daniel" && senha == "senhasupersecreta"){
-        alert('Sucesso');
-        location.href = "index.html";
-    }else{
-        alert('Usuario ou senha incorretos');
-    }
-
-}
-
-// esta segunda function é responsável por logar o usuário que já possuiuma conta no site
-
-// ainda vou fazer uma function que seja responsável por fazer as pesquisas do site, e também, aprimorar os que eu já fiz com o cadastramento do usuário!
+};
