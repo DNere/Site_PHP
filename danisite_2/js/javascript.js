6bd15dbd19d20d2d9753e1accf0b4cc24eefbad2
DNere <daniel.nere780@gmail.com>
2024-12-08 11:25:44 -0300

    javascript.js

--- danisite_2/js/javascript.js
+++ danisite_2/js/javascript.js
@@ -1,9 +1,17 @@
-// Mostra o botão ao descer a tela
+// O evento "onscroll" é acionado sempre que o usuário rolar a página
 window.onscroll = function() {
+    // Seleciona o elemento com a classe "botao-topo" na página
     const button = document.querySelector('.botao-topo');
+    
+    // Verifica a quantidade de deslocamento vertical da página
+    // "document.body.scrollTop" retorna o deslocamento vertical para navegadores mais antigos
+    // "document.documentElement.scrollTop" retorna o deslocamento vertical para navegadores modernos
+    // A condição verifica se o deslocamento ultrapassou 100 pixels
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        // Adiciona a classe "show" ao botão, geralmente para exibi-lo (dependendo do CSS aplicado)
         button.classList.add('show');
     } else {
+        // Remove a classe "show" do botão, geralmente para escondê-lo
         button.classList.remove('show');
     }
 };
