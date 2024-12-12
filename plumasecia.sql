9a3bb196a68f95be74363ada95dff23f9d24b452
DNere <daniel.nere780@gmail.com>
2024-12-12 19:46:51 -0300

    plumasecia.sql

--- plumasecia.sql
+++ plumasecia.sql
@@ -9,22 +9,3 @@ CREATE TABLE formulario (
 );
 
 SELECT * FROM formulario;
-
-INSERT INTO formulario VALUES 
-('001','daniel.nere780@gmail.com','Daniel Nere Rodrigues','senhasecreta'),
-('002','eduarda.rodrigues@gmail.com','Eduarda Nere Rodrigues','2004'),
-('003','a.ghiraldelli@aluno.ifsp.edu.br','Ana Julia Gomes Ghiraldelli','09shd32A638');
-
-
-
-SELECT formulario.nome AS 'Usuários', formulario.email AS 'Emails'
-	FROM formulario
-		ORDER BY formulario.nome;
-
-SELECT formulario.nome AS 'Usuários chamados Daniel'
-	FROM formulario
-		WHERE nome LIKE '%Daniel%';
-        
-SELECT formulario.email AS 'Emails'
-	FROM formulario
-		WHERE email LIKE '%gmail.com%';
